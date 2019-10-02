f2d408c5df3eb2442daa4d314338b976bff5f8f5
Paul Hong <paulhong1124@gmail.com>
2019-10-01 20:09:17 -0700

    about layout

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,12 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Home from './components/Home';
 import About from './components/About';
 import './App.css';
 
 function App() {
-  
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
   return (
     <div className="app">
         <Home />
--- client/src/components/About.jsx
+++ client/src/components/About.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import AboutImg from './images/about/main_pic.jpg';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faLinkedin, faGithub } from '@fortawesome/free-brands-svg-icons';
 import { faFilePdf } from '@fortawesome/free-solid-svg-icons';
@@ -9,8 +10,11 @@ export default function About() {
     return (
         <div id="about" className="about">
             <div className="about-intro">
-                <img className="about-img" src="" alt="main"/>
-                <p className="about-introduction">I design and develop simple and clean websites, and I love what I do.</p>
+                <img className="about-img" src={AboutImg} alt="main"/>
+                <div className="about-introduction">
+                    <h2 className="introduction-heading">Hey. I'm Paul.</h2>
+                    <p className="introduction-message">I design and develop simple and clean websites, and I love what I do. I'm always looking for opportunities to learn something as well as use what I have to design the best website. I've taken different routes to pursue throughout my whole life, and right now I'm fixated on full-stack development.</p>
+                </div>
             </div>
             <div className="about-main">
             </div>

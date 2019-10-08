da00d8596517571d61aed4a98653d456f8c2e59c
Paul Hong <paulhong1124@gmail.com>
2019-10-07 18:13:18 -0700

    new branch

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -17,44 +17,7 @@ export default function Home() {
         <section id="home" className="home" style={isMain ? mainStyle : null}>
             <div className="home-content">
                 <figure className="home-image"></figure>
-                <div className="home-main" style={{opacity: isMain ? 0 : 1}}>
-                    {/* <div className="home-main-content">
-                        <h2 className="home-title">
-                            Paul Hong
-                        </h2>
-                        <h4 className="home-subtitle">
-                            <div className="home-subtitle-underline"><span className="main-color">Full-Stack</span> Web Developer</div>
-                        </h4>
-                        <hr className="home-hr" />
-                        <div className="home-sub-content">
-                            <ul className="home-nav">
-                                <li><a href="#about">About</a></li>
-                                <li><a href="#project">Projects</a></li>
-                                <li><a href="#experience">Experience</a></li>
-                                <li><a href="#contact">Contact</a></li>
-                            </ul>
-                        </div>
-                        <hr className="home-hr-2nd" />
-                        <ul className="home-links">
-                            <li className="home-link">
-                                <a className="home-link-item" href="https://www.linkedin.com/in/paulhong931124">
-                                <FontAwesomeIcon icon={faLinkedin}/>
-                                </a>
-                            </li>
-                            
-                            <li className="home-link">
-                                <a className="home-link-item" href="https://www.github.com/paul1124">
-                                    <FontAwesomeIcon icon={faGithub}/>
-                                </a>
-                            </li>
-                            
-                            <li className="home-link">
-                                <a className="home-link-item" href="">
-                                    <FontAwesomeIcon icon={faFilePdf}/>
-                                </a>
-                            </li>
-                        </ul>
-                    </div> */}
+                <div className="home-main">
                     <div className="home-main-content">
                         <h2 className="home-title">
                             Paul Hong
